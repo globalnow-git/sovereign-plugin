bd38bd22d87ab29e6fda1e82fdb70c6475d62641
unknown <gregb@globalnow.net>
2026-06-10 08:01:00 -0700

    Remove tracked data/ directory and add gitignore - v2.6 plugin only

--- data/wordpress/wp-content/import.php
+++
@@ -1,6 +0,0 @@
-﻿<?php
-$file = "/var/www/html/wp-content/plugins/sovereign-builder/advanced/class-schema-designer.php";
-echo "Lines: " . substr_count(file_get_contents($file), "\n") . "\n";
-echo strpos(file_get_contents($file), "ob_start") !== false ? "HAS OB_START - broken\n" : "CLEAN\n";
-echo strpos(file_get_contents($file), "<<<JS") !== false ? "HAS HEREDOC - broken\n" : "NO HEREDOC\n";
-
